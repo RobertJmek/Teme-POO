e107aabd0ed5acad52f0c0d41143a6267646f1e2
RobertJmek <bunescurobert16@gmail.com>
2023-03-19 12:53:01 +0200

    Added the requirement

--- Tema1/proiect.cpp
+++ Tema1/proiect.cpp
@@ -79,7 +79,7 @@ public:
     {
         return med;
     }
-    Farmacie& operator+=(Medicament const& newmed)
+    Farmacie& operator+=(const Medicament& newmed)
     {   
         nrMedicamente++;
         Medicament *aux = new Medicament[nrMedicamente];
@@ -90,32 +90,35 @@ public:
         return *this;
 
     }
-    Farmacie& operator+(Medicament const& plusmed)
+    Farmacie& operator+(const Medicament& plusmed)
     {
         nrMedicamente++;
-        Medicament *aux = new Medicament[nrMedicamente + 1];
+        Medicament *aux = new Medicament[nrMedicamente];
         for ( int i=0; i < nrMedicamente - 1; i++ )
             aux[i] = med[i];
         aux[nrMedicamente - 1] = plusmed;
         med = aux;
         return *this;
     }
-    Farmacie& operator=(Farmacie const& farma)
+    Farmacie& operator=(const Farmacie& farma)
     {
+        if(this == &farma) return *this;
         strcpy( denumire, farma.denumire );
-        for ( int i=0; i < farma.nrMedicamente - 1; i++ )
+        med = new Medicament[farma.nrMedicamente];
+        for ( int i=0; i < farma.nrMedicamente; i++ )
         {   
             med[i] = farma.med[i];
         }
         nrMedicamente = farma.nrMedicamente;
+        return *this;
     }
     friend std::ostream& operator<<(std::ostream& os, const Farmacie& farma);
 };
 
 std::ostream& operator<<(std::ostream& os,const Farmacie& farma)
 {
-    for ( int i=0; i < farma.nrMedicamente - 1; i++ )
-        os<<farma.med[i]<<'\n';
+    for ( int i=0; i < farma.nrMedicamente; i++ )
+        os<<farma.med[i]<<' ';
     return os;
 }
 
@@ -124,18 +127,27 @@ int main()
   	Data d(1,1,2020);
     d.afisData();
     Medicament m1("Parasinus",9.5, 10, 12, 2019), m2("Aspirina",5.12, d),m3;
+    // colebil 6 12 2 1999
     std::cin>>m3;
     std::cout<<m1;
+    Data d1;
+    std::cin>>d1;
+    std::cout<<std::endl<<d1;
+    if ( d > d1 )
+        std::cout<<"d este mai tarziu decat d1"<<std::endl;
+    else 
+        std::cout<<"d este mai devreme sau aceeasi cu d1"<<std::endl;
     if (m1<m2) std::cout<<"m1 este fabricat inaintea m2"<<'\n';
-    else std::cout<<"m2 este fabricat inaintea m1";
+    else std::cout<<"m2 este fabricat inaintea m1"<<std::endl;
     Farmacie f1("Farmac");
     Farmacie f2;
     std::cout<<f1.getNume()<<'\n';
     f1 += m1;
     f1 += m2;
     f1 += m3;
+    std::cout<<f1<<std::endl;
     f2 = f2 + m1; //adaugare medicament m2 in lista de medicamente a farmaciei
-    std::cout<<f2;
+    std::cout<< f2;
     f2 = f1;
     std::cout<<f2;	//afisarea tuturor medicamentelor
     return 0;
